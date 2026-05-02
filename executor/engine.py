7aeed8cd75599961001139c66e8a89648c9c33fd
Bafciorz <jbafia@student.agh.edu.pl>
2026-05-02 12:45:41 +0200

    create

--- executor/engine.py
+++ executor/engine.py
@@ -6,6 +6,8 @@ from errors.errors import SqlSyntaxError
 from parser.parser import parser
 from lexer.lexer import lexer
 import csv
+import json
+import os
 
 class CSVEngine:
     query : str
@@ -38,7 +40,7 @@ class CSVEngine:
                         response = self.insert(query)
                     case 'CREATE':
                         print("do implementacji")
-                        # response = self.create(query)
+                        response = self.create(query)
                     case 'DROP':
                         print("do implementacji")
                         # response = self.drop(query)
@@ -58,31 +60,48 @@ class CSVEngine:
         if conditions is None:
             return True
 
-        type = conditions[0]
-        if type == 'LOGIC':
+    
+        cond_type = conditions[0]
+
+        if cond_type == 'LOGIC':
             _, operator, left, right = conditions
             if operator == 'AND':
-                return self.check_condition(row,left) and self.check_condition(row,right)
+                return self.check_condition(row, left) and self.check_condition(row, right)
             else:
-                return self.check_condition(row,left) or self.check_condition(row,right)
-        elif type == 'RELATION':
-            _, operator,column , value = conditions
+                return self.check_condition(row, left) or self.check_condition(row, right)
 
+        elif cond_type == 'RELATION':
+            _, operator, column, value = conditions
             cell_value = row.get(column)
+
+            if cell_value is None or cell_value == "":
+                return False
 
             try:
                 if isinstance(value, (int, float)):
-                    cell_value = float(cell_value)
-            except (ValueError, TypeError):
-                pass
+                    actual_cell_value = float(cell_value)
+                    actual_query_value = float(value)
+                else:
+                    actual_cell_value = str(cell_value)
+                    actual_query_value = str(value)
 
-            match operator:
-                case '<': return cell_value < value
-                case '<=': return cell_value <= value
-                case '>': return cell_value > value
-                case '>=': return cell_value >= value
-                case '=' : return cell_value == value
-                case '!=' : return cell_value != value
+
+                match operator:
+                    case '<':
+                        return actual_cell_value < actual_query_value
+                    case '<=':
+                        return actual_cell_value <= actual_query_value
+                    case '>':
+                        return actual_cell_value > actual_query_value
+                    case '>=':
+                        return actual_cell_value >= actual_query_value
+                    case '=':
+                        return actual_cell_value == actual_query_value
+                    case '!=':
+                        return actual_cell_value != actual_query_value
+
+            except (ValueError, TypeError):
+                return False
 
         return False
 
@@ -156,57 +175,100 @@ class CSVEngine:
     #         print(" | ".join(str(row.get(col, "")) for col in header))
 
     def insert(self, query):
-        table_name : str
         table_name = query['table_name']
-        columns = query['columns']
-        values = query['values']
-
         if not table_name.lower().endswith('.csv'):
             table_name += '.csv'
+
+        columns = query['columns']
+        values = query['values']
+        meta_path = table_name + ".meta.json"
 
         try:
             with open(table_name, mode='r', encoding='utf8') as f:
                 reader = csv.reader(f)
                 file_headers = next(reader)
 
-            for col in columns:
-                if col not in file_headers:
-                    return QueryResponse(status='error', message=f"Kolumna '{col}' nie istnieje w {table_name}",
-                                         data=None)
+            metadata = None
+            if os.path.exists(meta_path):
+                with open(meta_path, 'r', encoding='utf8') as f:
+                    metadata = json.load(f)
+
             rows_to_write = []
             for i, row in enumerate(values):
                 if len(columns) != len(row):
-                    return QueryResponse(
-                        status = 'error',
-                        message = f'Błąd w rekordzie nr {i+1}. Niepoprawna ilość argumentów. Podano {len(row)} a oczekiwano {len(columns)}',
-                        data = None
-                    )
-                row_map = dict(zip(columns,row))
-                mapped_values = [row_map.get(header,'') for header in file_headers]
+                    return QueryResponse(status='error',
+                                         message=f'Błąd w rekordzie {i + 1}: Liczba wartości się nie zgadza.',
+                                         data=None)
+
+                row_map = dict(zip(columns, row))
+                if metadata:
+                    for col_name, val in row_map.items():
+                        col_def = metadata['columns'].get(col_name)
+                        if not col_def:
+                            continue
+
+                        if col_def['type'] == 'VARCHAR' and col_def.get('length'):
+                            if len(str(val)) > col_def['length']:
+                                return QueryResponse(status='error',
+                                                     message=f"Błąd: Wartość '{val}' przekracza długość {col_def['length']} dla kolumny {col_name}",
+                                                     data=None)
+                        if col_def['type'] == 'NUMERIC':
+                            try:
+                                float(val)
+                            except ValueError:
+                                return QueryResponse(status='error',
+                                                     message=f"Błąd: Kolumna {col_name} wymaga liczby, otrzymano '{val}'",
+                                                     data=None)
+
+                mapped_values = []
+                for header in file_headers:
+                    if header in row_map:
+                        mapped_values.append(row_map[header])
+                    elif metadata and metadata['columns'].get(header) and metadata['columns'][header].get('default'):
+                        mapped_values.append(metadata['columns'][header]['default'])
+                    else:
+                        mapped_values.append('')
+
                 rows_to_write.append(mapped_values)
 
-            with open(table_name, mode = 'a', newline='', encoding = 'utf8' ) as file:
+            with open(table_name, mode='a', newline='', encoding='utf8') as file:
                 writer = csv.writer(file)
                 writer.writerows(rows_to_write)
-            return QueryResponse(
-                status = 'success',
-                message = 'Pomyślnie dodano podane wartości',
-                data = None
-            )
 
-
-        except FileNotFoundError:
-
-            return QueryResponse(status='error', message=f"Nie znaleziono pliku: {table_name}", data=None)
+            return QueryResponse(status='success', message=f'Pomyślnie dodano {len(rows_to_write)} wierszy.',
+                                 data=None)
 
         except Exception as e:
-
-            return QueryResponse(status='error', message=f"Błąd krytyczny zapisu: {str(e)}", data=None)
-
+            return QueryResponse(status='error', message=f"Błąd INSERT: {str(e)}", data=None)
 
 
     def create(self,query):
-        print(query)
+        table_name = query['table_name']
+        if not table_name.lower().endswith('.csv'):
+            table_name += '.csv'
+        metadata = {
+            "table_name": table_name,
+            "columns": {col['identifier']: col for col in query['columns']}
+        }
+
+        try:
+            headers = list(metadata["columns"].keys())
+            with open(table_name, 'w', newline='', encoding='utf8') as f:
+                writer = csv.writer(f)
+                writer.writerow(headers)
+
+            meta_file = table_name + ".meta.json"
+            with open(meta_file, 'w', encoding='utf8') as f:
+                json.dump(metadata, f, indent=4)
+
+            return QueryResponse(
+                status='success',
+                message=f"Utworzono tabelę {table_name} oraz plik metadanych.",
+                data=None
+            )
+
+        except Exception as e:
+            return QueryResponse(status='error', message=f"Błąd CREATE: {str(e)}", data=None)
 
     def drop(self,query):
         print(query)
